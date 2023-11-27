56d6dde9b4d40a06fc2aa533046c4277e05c9d66
Deorren Erive <mac@Deorrens-MacBook-Air.local>
2023-11-27 05:37:39 -0800

    x

--- app/components/Tasks/DisplayTasks.tsx
+++ app/components/Tasks/DisplayTasks.tsx
@@ -10,8 +10,8 @@ type DisplayProps = {
     tasks: TaskProps[]
 }
 
-export default function DisplayTasks({tasks}:DisplayProps) {
-    
+export default function DisplayTasks({ tasks }: DisplayProps) {
+
     // return (
     //     <div className="">
     //         {tasks ? tasks.map((task) => (
@@ -39,65 +39,65 @@ export default function DisplayTasks({tasks}:DisplayProps) {
     //                 <span className="loading loading-ring loading-lg"></span>
     //             </div>
     //     }
-    
+
     //     </div>
     //   )
 
     return (
         <div>
             {tasks.length !== 0 ?
-             <div className="overflow-x-auto">
-                <table className="table ">
-                 {/* head */}
-                 <thead>
-                 <tr className='font-bold text-black'>
-                     <th></th>
-                     <th>Task</th>
-                     <th>Progress</th>
-                     <th>Actions</th>
-                 </tr>
-                 </thead>
-                 <tbody>
-                 {/* row 1 */}
-                 {tasks && tasks.map((task, index) => (
-                     <tr key={index}>
-                         <th>{index + 1}</th>
-                             <td>
-                                 <Link href={`/task/${task._id}`} className='font-bold'>
-                                     {task.title}
-                                 </Link>                    
-                             </td>
-                             <td>
+                <div className="overflow-x-auto">
+                    <table className="table ">
+                        {/* head */}
+                        <thead>
+                            <tr className='font-bold text-black'>
+                                <th></th>
+                                <th>Task</th>
+                                <th>Progress</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
+                            {tasks && tasks.map((task, index) => (
+                                <tr key={index}>
+                                    <th>{index + 1}</th>
+                                    <td className='w-20 overflow-x-auto'>
+                                        <Link href={`/task/${task._id}`} className='font-bold'>
+                                            {task.title}
+                                        </Link>
+                                    </td>
+                                    <td>
 
-                             <div className={`badge 
-                             ${task.status === "Pending" ? "badge-warning" : "" }
-                             ${task.status === "Completed" ? "badge-success" : "" }
-                             ${task.status === "Not started" ? "badge-error" : "" }
-                             text-white`}>
-                                 {task.status}
-                             </div>
-                             </td>
-                             <td className='flex justify-around'>
-                                 <Link href={`/edit-task/${task._id}`} className="text-orange-300 text-xl">
-                                     <BiEditAlt />
-                                 </Link>
-                                   <DeleteTask id={task._id}/>
-                             </td>
-                     </tr>
-                 ))}
-                
-               
-                 </tbody>
-             </table>
-             </div>
-             :
+                                        <div className={`badge 
+                                        ${task.status === "Pending" ? "badge-warning" : ""}
+                                        ${task.status === "Completed" ? "badge-success" : ""}
+                                        ${task.status === "Not started" ? "badge-error" : ""}
+                                        text-white`}>
+                                            {task.status}
+                                        </div>
+                                    </td>
+                                    <td className='flex justify-around items-center'>
+                                        <Link href={`/edit-task/${task._id}`} className="text-orange-300 text-xl">
+                                            <BiEditAlt />
+                                        </Link>
+                                        <DeleteTask id={task._id} />
+                                    </td>
+                                </tr>
+                            ))}
+
+
+                        </tbody>
+                    </table>
+                </div>
+                :
                 <div className='flex flex-col justify-center items-center h-20'>
                     <h1 className='font-light'>No task found..</h1>
                 </div>
-        
-        }
-           
+
+            }
+
         </div>
     )
-  
+
 }
